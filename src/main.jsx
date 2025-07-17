5e2a9c027a04c413163dff92091e8031ebfd4590
pattapu-22 <venkateswarammapattapu4322@gmail.com>
2025-07-17 22:38:45 +0530

    removed the my-portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,47 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import Gallery from './Gallery';
+// // import { StrictMode } from 'react'
+// // import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// // import App from './App.jsx'
+// // import Gallery from './Gallery';
 
+
+// // createRoot(document.getElementById('root')).render(
+// //   <StrictMode>
+// //     <App />
+// //   </StrictMode>,
+// // )
+
+// import React, { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// import App from './App.jsx'; // Home page
+// import Gallery from './Gallery.jsx';
 
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
-//     <App />
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/gallery" element={<Gallery />} />
+//       </Routes>
+//     </Router>
 //   </StrictMode>,
-// )
+// );
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './App.jsx'; // Home page
 import Gallery from './Gallery.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename="/portfolio">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/gallery" element={<Gallery />} />
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  
   plugins: [react(),
     tailwindcss(),
   ],
+  base: '/portfolio/',
 })
